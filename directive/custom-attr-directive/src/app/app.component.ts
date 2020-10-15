3d7927bd90369cc59a86ca05a34b1568a3134655
Yetaeng <yetaeng20@gmail.com>
2020-10-15 13:56:53 +0900

    사용자 정의 어트리뷰트 디렉티브

--- directive/custom-attr-directive/src/app/app.component.ts
+++ directive/custom-attr-directive/src/app/app.component.ts
@@ -5,9 +5,23 @@ import { Component } from '@angular/core';
   // textBlue 디렉티브를 p 요소에 어트리뷰트로 적용하면, 이제 p 요소는 어트리뷰트 호스트 요소가 된다.
   // template: '<p textColor [color]="color">Text Color</p>', 프로퍼티 바인딩 방법
   //template: '<p [textColor]="color">Text Color</p>', // 디렉티브에 직접 바인딩
-  template:
-    '<button myDirective [inputValue]="msg" staticValue="hi!">Click me</button>',
+  // template:
+  //   '<button myDirective [inputValue]="msg" staticValue="hi!">Click me</button>',
+  template: `
+    <h1>Attribute Directive</h1>
+
+    <h4>텍스트의 컬러를 선택하세요</h4>
+    <div>
+      <input type="radio" name="colors" (click)="color = 'red'" />Red
+      <input type="radio" name="colors" (click)="color = 'blue'" />Blue
+      <input type="radio" name="colors" (click)="color = 'green'" />Green
+    </div>
+
+    <p [textColor]="color" defaultColor="violet">Text Color</p>
+  `,
 })
 export class AppComponent {
-  color = 'button click'; // 동적으로 값 전달
+  // color = 'button click'; // 동적으로 값 전달
+  // 라디오 버튼에 의해 지정된 텍스트 컬러로 프로퍼티 바인딩에 의해 디렉티브로 전달됨
+  color = 'string';
 }
--- directive/custom-attr-directive/src/app/text-color.directive.ts
+++ directive/custom-attr-directive/src/app/text-color.directive.ts
@@ -5,6 +5,7 @@ import {
   ElementRef,
   Renderer2,
   HostListener,
+  OnInit,
 } from '@angular/core';
 import { tick } from '@angular/core/testing';
 
@@ -14,15 +15,23 @@ import { tick } from '@angular/core/testing';
 export class TextColorDirective {
   // 호소트 요소에서 프로퍼티 바인딩한 값을 전달받음
   @Input('textColor') color: string; // 1. 바인딩한 컬러가
+  // 일반 어트리뷰트의 정적 값의 전달
+  @Input() defaultColor: string;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  // 기본 컬러를 초기 표시에 적용
+  ngOnInit() {
+    this.textColor(this.defaultColor);
+  }
 
   @HostListener('mouseenter') handleMouseEnter() {
     this.textColor(this.color); // 3. 호스트 요소의 텍스트 컬러가 이벤트에 의해 변경됨
   }
 
   @HostListener('mouseleave') handleMouseLeave() {
-    this.textColor(null);
+    // this.textColor(null);
+    this.textColor(this.defaultColor);
   }
 
   private textColor(color: string) {
