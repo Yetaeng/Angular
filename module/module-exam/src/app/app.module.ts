7f02e0e91afbe6e52a2b717b38ce72c1bfc48d21
Yetaeng <yetaeng20@gmail.com>
2020-11-05 14:57:13 +0900

    핵심 모듈 분리

--- module/module-exam/src/app/app.module.ts
+++ module/module-exam/src/app/app.module.ts
@@ -7,9 +7,12 @@ import { AppComponent } from './app.component';
 // import { HeaderComponent } from './shared/header.component'; 공유 모듈에 등록되었으므로 제거
 // import { HomeComponent } from './home/home.component'; HomeModule에 등록되어있는거니까 더 이상 루트 모듈의 관리 대상이 아님 -> 삭제(declarations에 있는 것도 제외시킴)
 
+// CoreModule 임포트
+import { CoreModule } from './core/core.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HomeModule /* 임포트 */],
+  imports: [BrowserModule, HomeModule /* 임포트 */, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
